7f01143910e5c079612507868e1d22787fd6dfbe
jefferyrlowe <56458009+jefferyrlowe@users.noreply.github.com>
2019-10-11 19:48:30 -0400

    Create locked_objects.sql

---
+++ locked_objects.sql
@@ -0,0 +1,21 @@
+SELECT l.inst_id,SUBSTR(L.ORACLE_USERNAME,1,8) ORA_USER, SUBSTR(L.SESSION_ID,1,5) SID,
+S.serial#,
+p.pname,p.spid,
+SUBSTR(O.OWNER||'.'||O.OBJECT_NAME,1,40) OBJECT, P.SPID OS_PID,
+DECODE(L.LOCKED_MODE, 0,'NONE',
+1,'NULL',
+2,'ROW SHARE',
+3,'ROW EXCLUSIVE',
+4,'SHARE',
+5,'SHARE ROW EXCLUSIVE',
+6,'EXCLUSIVE',
+NULL) LOCK_MODE,
+s.logon_time,
+s.seconds_in_wait
+FROM sys.GV_$LOCKED_OBJECT L, DBA_OBJECTS O, sys.GV_$SESSION S, sys.GV_$PROCESS P
+WHERE L.OBJECT_ID = O.OBJECT_ID
+and l.inst_id = s.inst_id
+AND L.SESSION_ID = S.SID
+and s.inst_id = p.inst_id
+and S.PADDR = P.ADDR(+)
+order by l.inst_id;
